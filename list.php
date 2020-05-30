ea103b067b3cc2a8cc670b69e3da9ea74860a240
sivanimodali <sivanimodali@gmail.com>
2020-05-30 14:38:39 +0200

    list of devices

---
+++ list.php
@@ -0,0 +1,12 @@
+<?php
+
+include('config.php');
+
+$result = $db->query('SELECT * FROM List');
+
+while($row = $result->fetchArray(SQLITE3_ASSOC) ) {
+	echo  $row['IP']. "|" .$row['VLANs']. "|" .$row['PORT']. "|" .$row['MACS']. "\n";
+}
+$db->close();
+
+?>
